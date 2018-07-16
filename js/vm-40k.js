65d410bbcb4347c64880b91675256827b3f2d661
FreeAlchemist <freealchemist@mail.ru>
2018-07-16 17:48:39 +0300

    Add files via upload

--- js/vm-40k.js
+++ js/vm-40k.js
@@ -40,43 +40,70 @@
 //   keywords:'CHARACTER, INFANTRY, CYPHER',
 // }
 
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
-
-
-
 Vue.component('modelstats', {
   props: ['model'],
-  template: `<table>
+  template: `<table class="topborder" cellpadding="0px" cellspacing="0px">
  <tr>
-   <td>Move</td>
-   <td>WS</td>
-   <td>BS</td>
-   <td>S</td>
-   <td>T</td>
-   <td>W</td>
-   <td>A</td>
-   <td>LD</td>
-   <td>SV</td>
-   <td>INSV</td>
+   <td class="stats-header">Move</td>
+   <td class="stats-header">WS</td>
+   <td class="stats-header">BS</td>
+   <td class="stats-header">S</td>
+   <td class="stats-header">T</td>
+   <td class="stats-header">W</td>
+   <td class="stats-header">A</td>
+   <td class="stats-header">LD</td>
+   <td class="stats-header">SV</td>
+   <td class="stats-header">INSV</td>
  </tr>
  <tr>
-   <td>{{model.move}}</td>
-   <td>{{model.ws}}</td>
-   <td>{{model.bs}}</td>
-   <td>{{model.strength}}</td>
-   <td>{{model.toughness}}</td>
-   <td>{{model.wounds}}</td>
-   <td>{{model.attacks}}</td>
-   <td>{{model.leadership}}</td>
-   <td>{{model.save}}</td>
-   <td>{{model.invulsave}}</td>
+   <td  class="stats-text">{{model.move}}</td>
+   <td  class="stats-text">{{model.ws}}</td>
+   <td  class="stats-text">{{model.bs}}</td>
+   <td  class="stats-text">{{model.strength}}</td>
+   <td  class="stats-text">{{model.toughness}}</td>
+   <td  class="stats-text">{{model.wounds}}</td>
+   <td  class="stats-text">{{model.attacks}}</td>
+   <td  class="stats-text">{{model.leadership}}</td>
+   <td  class="stats-text">{{model.save}}</td>
+   <td  class="stats-text">{{model.invulsave}}</td>
+ </tr>
+ </table>`
+})
+
+Vue.component('weaponstats', {
+  props: ['weapon','weapons'],
+  template: `<tr>
+   <td  class="stats-text">{{weapon}}</td>
+   <td  class="stats-text">{{weapons[weapon].range}}</td>
+   <td  class="stats-text">{{weapons[weapon].type}}</td>
+   <td  class="stats-text">{{weapons[weapon].strength}}</td>
+   <td  class="stats-text">{{weapons[weapon].ap}}</td>
+   <td  class="stats-text">{{weapons[weapon].damage}}</td>
+   <td  class="stats-text">{{weapons[weapon].ability}}</td>
+   <td  class="stats-text">{{weapons[weapon].points}}</td>
+ </tr>`
+})
+
+Vue.component('modelabilities', {
+  props: ['ability','abilitytext'],
+  template: `<table class="topborder" cellpadding="0px" cellspacing="0px">
+ <tr>
+   <td class="abilityheader">{{ability}}</td>
+   <td class="abilitytext">{{abilitytext}}</td>
+ </tr>
+ </table>`
+})
+
+Vue.component('modelkeywords', {
+  props: ['model'],
+  template: `<table class="topborder" cellpadding="0px" cellspacing="0px">
+ <tr>
+   <td class="abilityheader table-header">FACTION KEYWORDS</td>
+   <td class="abilitytext">{{model.factionkeywords}}</td>
+ </tr>
+ <tr>
+   <td class="abilityheader table-header">KEYWORDS</td>
+   <td class="abilitytext">{{model.keywords}}</td>
  </tr>
  </table>`
 })
@@ -85,190 +112,57 @@ var builder = new Vue({
   el: '#builder',
   data: {
     armies:library,
-    units:library.fallenangels.units,
+    // units:library.fallenangels.units,
+    units:library.inquisition.units,
     selected: 'inquisition',
-    // army:armies[{{selected}}],
-    // units:army.units,
-  }
+    army:Object,
+    weapons:Object,
+    abilities:Object,
+    specialrules:Object,
+    traits:Object,
+    relics:Object,
+    adaptation:Object,
+    magic:Object,
+    units:Object,
+  },
+  methods:{
+    getArmy: function (){
+      this.army = library[this.selected]
+      // console.log('army')
+      // console.log(this.army)
+      // console.log('')
+      // console.log('weapons')
+      this.weapons = library[this.selected].weapons
+      // console.log(this.weapons)
+      // console.log('')
+      // console.log('abilities')
+      this.abilities = library[this.selected].abilities
+      // console.log(this.abilities)
+      // console.log('')
+      // console.log('specialrules')
+      this.specialrules = library[this.selected].specialrules
+      // console.log(this.specialrules)
+      // console.log('')
+      // console.log('traits')
+      this.traits = library[this.selected].traits
+      // console.log(this.traits)
+      // console.log('')
+      // console.log('relics')
+      this.relics = library[this.selected].relics
+      // console.log(this.relics)
+      // console.log('')
+      // console.log('adaptation')
+      this.adaptation = library[this.selected].adaptation
+      // console.log(this.adaptation)
+      // console.log('')
+      // console.log('magic')
+      this.magic = library[this.selected].magic
+      // console.log(this.magic)
+      // console.log('')
+      // console.log('units')
+      this.units = library[this.selected].units
+      // console.log(this.units)
+      // console.log('')
+    },
+  },
 })
-
-// var unitlist = new Vue({
-//   el: '#unitlist',
-//   data: {
-//     // armies:library,
-//     // army:library.fallenangels,
-//     // units:library.fallenangels.units,
-//     // ourmodel:library.fallenangels.units.cypher
-//   }
-// })
-
-/*
-<div class="chars">
-  <div class="name">
-
-  </div>
-  <table cellpadding="0px" cellspacing="0px" width="100%" class="topborder">
-    
-  </table>
-</div>
-<div class="weapon">
-
-</div>
-<div class="abilities">
-
-</div>
-<div class="keywords">
-
-</div>
-
-
-
-
-
-
-
- var table = $('<table />')
-table.attr('cellpadding',"0px")
-table.attr('cellspacing','0px')
-table.attr('width','100%')
-table.attr('class','topborder')
-table.attr('id',rosterarr[i]+'-'+i+'-statstable')
-$('#'+rosterarr[i]+'-'+i+'-chars').append(table)
-var tr = $('<tr />');
-var td1 = $('<td />',{class:'weapon-stats-header',text:'move"'})
-var td2 = $('<td />',{class:'weapon-stats-header',text:'WS'})
-var td3 = $('<td />',{class:'weapon-stats-header',text:'BS'})
-// var td4 = $('<td />',{class:'weapon-stats-header smallstat',text:'S'})
-var td4 = $('<td />',{class:'weapon-stats-header',text:'S'})
-var td5 = $('<td />',{class:'weapon-stats-header smallstat',text:'T'})
-var td6 = $('<td />',{class:'weapon-stats-header',text:'W'})
-var td7 = $('<td />',{class:'weapon-stats-header',text:'A'})
-var td8 = $('<td />',{class:'weapon-stats-header smallstat',text:'Ld'})
-var td9 = $('<td />',{class:'weapon-stats-header',text:'Sv'})
-var td10 = $('<td />',{class:'weapon-stats-header',text:'InvSv'})
-var statsinfo = tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6).append(td7).append(td8).append(td9).append(td10)
-$('#'+rosterarr[i]+'-'+i+'-statstable').append(statsinfo)
-var tr = $('<tr />');
-var td1 = $('<td />',{class:'weapon-stats-text',text:move})
-var td2 = $('<td />',{class:'weapon-stats-text',text:ws})
-var td3 = $('<td />',{class:'weapon-stats-text',text:bs})
-var td4 = $('<td />',{class:'weapon-stats-text smallstat',text:strength})
-var td5 = $('<td />',{class:'weapon-stats-text smallstat',text:toughness})
-var td6 = $('<td />',{class:'weapon-stats-text',text:wounds})
-var td7 = $('<td />',{class:'weapon-stats-text',text:attacks})
-var td8 = $('<td />',{class:'weapon-stats-text smallstat',text:leadership})
-var td9 = $('<td />',{class:'weapon-stats-text',text:save})
-var td10 = $('<td />',{class:'weapon-stats-text',text:invulsave})
-var statsinfo = tr.append(td1).append(td2).append(td3).append(td4).append(td5).append(td6).append(td7).append(td8).append(td9).append(td10)
-$('#'+rosterarr[i]+'-'+i+'-statstable').append(statsinfo)
-*/
-
-
-
-
-
-
-// // example 1
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Привет, мир Vue!'
-//   }
-// })
-
-// // example 2
-// // v-bind
-// var app2 = new Vue({
-//   el: '#app-2',
-//   data: {
-//     message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
-//   }
-// })
-
-// // example 3
-// // v-if
-// var app3 = new Vue({
-//   el: '#app-3',
-//   data: {
-//     seen: true
-//   }
-// })
-
-// // example 4
-// // v-for
-// var app4 = new Vue({
-//   el: '#app-4',
-//   data: {
-//   	message: 'Дела на сегодня',
-//     todos: [
-//       { text: 'Изучить JavaScript' },
-//       { text: 'Изучить Vue', color:'red'},
-//       { text: 'Создать что-нибудь классное', color:'Skyblue' }
-//     ]
-//   }
-// })
-
-// // example 5
-// // v-on
-// var app5 = new Vue({
-//   el: '#app-5',
-//   data: {
-//     message: 'Отзеркаленный текст становится музыкой.'
-//   },
-//   methods: {
-//     reverseMessage: function () {
-//       this.message = this.message.split('').reverse().join('')
-//     }
-//   }
-// })
-
-// // example 6
-// // v-model
-// var app6 = new Vue({
-//   el: '#app-6',
-//   data: {
-//     message: 'Введенный вами текст!'
-//   }
-// })
-
-// // todo list
-// Vue.component('todo-item', {
-//   template: '\
-//     <li>\
-//       {{ title }}\
-//       <button v-on:click="$emit(\'remove\')">Удалить</button>\
-//     </li>\
-//   ',
-//   props: ['title']
-// })
-
-// new Vue({
-//   el: '#todo-list-example',
-//   data: {
-//     newTodoText: '',
-//     todos: [
-//       {
-//         id: 1,
-//         title: 'Помыть посуду'
-//       },
-//       {
-//         id: 2,
-//         title: 'Вынести мусор'
-//       },
-//       {
-//         id: 3,
-//         title: 'Подстричь газон'
-//       }
-//     ],
-//     nextTodoId: 4
-//   },
-//   methods: {
-//     addNewTodo: function () {
-//       this.todos.push({
-//         id: this.nextTodoId++,
-//         title: this.newTodoText
-//       })
-//       this.newTodoText = ''
-//     }
-//   }
-// })
